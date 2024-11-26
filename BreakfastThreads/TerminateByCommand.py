090d4bdd12856c0c0cfd1c858c86bfb369d3023b
dryanakiev <dyanakiev@codingburgas.bg>
2024-11-26 15:58:57 +0200

    Simulate proper program termination

--- BreakfastThreads/TerminateByCommand.py
+++ BreakfastThreads/TerminateByCommand.py
@@ -1,15 +1,21 @@
 import time
 import threading
 
+thread_stop = False
 counter = 0
 
 def read_from_database():
+    global thread_stop
     global counter
+
     while True:
+        if thread_stop:
+            break
+
         counter += 1
         time.sleep(1)
 
-something_thread = threading.Thread(target=read_from_database,daemon=True)
+something_thread = threading.Thread(target=read_from_database)
 
 something_thread.start()
 
@@ -17,12 +23,16 @@ print("1.Check reading status")
 print("2.End program")
 
 while True:
+    if thread_stop:
+        break
+
     match input("Choice: "):
         case "1":
             print(f"Database has read {counter} lines")
         case "2":
-            break
+            thread_stop = True
         case _:
             "Wrong input"
 
-print("Interrupting program")
+print("Stopping program")
+print(f"Thread has finished with status code: {something_thread.is_alive()}")
